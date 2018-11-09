16e27275089f812e9b56c6bd703953780e9ac2b0
revoljucioner <moderya7@gmail.com>
2018-11-10 01:53:36 +0200

    Add ignored

---
+++ Log/Log/Elements/CustomMap .cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Log.Models;
+using Xamarin.Forms.Maps;
+
+namespace Log.Elements
+{
+    public class CustomMap : Map
+    {
+        public List<PolylineSegment> PolylineSegmentList { get; set; }
+    }
+}
---
+++ Log/Log/Elements/SpeedColorBox.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace Log.Elements
+{
+    public class SpeedColorBox : StackLayout
+    {
+        public SpeedColorBox(Color color, string text)
+        {
+            Orientation = StackOrientation.Horizontal;
+            Padding = new Thickness (5, 0, 0, 5);
+
+            var colorBox = new BoxView
+            {
+                Color = color,
+                WidthRequest = 20,
+                HeightRequest = 20
+            };
+
+            var speedLabel = new Label {FontSize = 15, Text = text};
+
+            Children.Add(colorBox);
+            Children.Add(speedLabel);
+        }
+    }
+}
