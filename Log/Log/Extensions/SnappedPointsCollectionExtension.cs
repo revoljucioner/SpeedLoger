86ff3f8608079d55e88b9963303989b74f9ce12c
revoljucioner <moderya7@gmail.com>
2018-11-11 20:58:28 +0200

    Add ignored files

---
+++ Log/Log/Extensions/SnappedPointsCollectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Log.Models;
+
+namespace Log.Extensions
+{
+    public static class SnappedPointsCollectionExtension
+    {
+        public static List<PolylineSegment> ToPolylineSegmentList(this IEnumerable<SnappedPoint> snappedPointsCollection)
+        {
+            var polylineSegmentList = new List<PolylineSegment>();
+
+            var snappedPointsArray = snappedPointsCollection.ToArray();
+
+            for (var i = 1; i < snappedPointsArray.Length; i++)
+            {
+                var snappedPointStart = snappedPointsArray[i - 1];
+                var snappedPointEnd = snappedPointsArray[i];
+
+                polylineSegmentList.Add(new PolylineSegment(snappedPointStart, snappedPointEnd));
+            }
+
+            return polylineSegmentList;
+        }
+
+    }
+}
---
+++ Log/Log/Models/PolylineSegment.cs
@@ -0,0 +1,30 @@
+﻿using System.Device.Location;
+
+namespace Log.Models
+{
+    public class PolylineSegment
+    {
+        public SnappedPoint SnappedPointStart { get; set; }
+        public SnappedPoint SnappedPointEnd { get; set; }
+
+        public PolylineSegment(SnappedPoint snappedPointStart, SnappedPoint snappedPointEnd)
+        {
+            SnappedPointStart = snappedPointStart;
+            SnappedPointEnd = snappedPointEnd;
+        }
+
+        public double SpeedBetweenPoints()
+        {
+            var geoCoordinateStart = new GeoCoordinate(SnappedPointStart.Position.Latitude, SnappedPointStart.Position.Longitude);
+            var geoCoordinateEnd = new GeoCoordinate(SnappedPointEnd.Position.Latitude, SnappedPointEnd.Position.Longitude);
+
+            // km
+            var distance = geoCoordinateStart.GetDistanceTo(geoCoordinateEnd) / 1000;
+            // h
+            var hoursTime = (SnappedPointEnd.Time - SnappedPointStart.Time).TotalHours;
+            // km/h
+            var speed = distance / hoursTime;
+            return speed;
+        }
+    }
+}
