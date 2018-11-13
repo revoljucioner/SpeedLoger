d86415dcaa3107498324d736259a4d4801cef37f
revoljucioner <moderya7@gmail.com>
2018-11-13 16:19:58 +0200

    Add carousel page that use animationView

---
+++ Log/Log/Pages/MainPageCarousel2.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Log.Pages
+{
+	public partial class MainPageCarousel2 : CarouselPage
+	{
+		public MainPageCarousel2()
+		{
+			InitializeComponent ();
+		}
+
+	    #region activities
+
+	    private async void ButtonLottie_Clicked(object sender, EventArgs e)
+	    {
+	        //await Navigation.PushAsync(new RecordsListPage(), true);
+	    }
+
+	    #endregion
+    }
+}
