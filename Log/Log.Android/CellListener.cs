671e35b0227ddc7df799e9e1fd3a8eaf161a2113
revoljucioner <moderya7@gmail.com>
2018-11-20 22:20:14 +0200

    Add managers

---
+++ Log/Log.Android/CellListener.cs
@@ -0,0 +1,33 @@
+﻿using Android.Content;
+using Android.Net;
+using Android.Telecom;
+using Android.Telephony;
+using Log.DependenciesOS;
+using Log.Droid;
+using Xamarin.Forms;
+
+[assembly: Xamarin.Forms.Dependency(typeof(CellListener))]
+namespace Log.Droid
+{
+    public class CellListener :ICellAnalyzer
+    {
+        private TelephonyManager _telephonyManager;
+        public CellListener()
+        {
+            _telephonyManager = (TelephonyManager)Forms.Context.GetSystemService(Context.TelephonyService);
+        }
+
+        public int GetGsmSignalStrength()
+        {
+            ConnectivityManager cm = (ConnectivityManager)Forms.Context.GetSystemService(Context.ConnectivityService);
+            var y = cm.ActiveNetworkInfo;
+            //
+            TelecomManager cm2 = (TelecomManager)Forms.Context.GetSystemService(Context.TelecomService);
+            //var y2 = cm2.IsInManagedCall;
+            //
+            var gsmSignalStrength = _telephonyManager.SignalStrength.GsmSignalStrength;
+            //var ttt2 = ttt.GsmSignalStrength;
+            return gsmSignalStrength;
+        }
+    }
+}
