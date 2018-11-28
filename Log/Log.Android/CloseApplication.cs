495a3f090e7de5fcbb7f7296b468b997f2170908
revoljucioner <moderya7@gmail.com>
2018-11-28 14:36:36 +0200

    ignored

---
+++ Log/Log.Android/CloseApplication.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Log.DependenciesOS;
+using Log.Droid;
+using Xamarin.Forms;
+
+[assembly: Xamarin.Forms.Dependency(typeof(CloseApplication))]
+namespace Log.Droid
+{
+    public class CloseApplication : ICloseApplication
+    {
+        public void TerminateApplication()
+        {
+            var activity = (Activity)Forms.Context;
+            activity.FinishAffinity();
+        }
+    }
+}
