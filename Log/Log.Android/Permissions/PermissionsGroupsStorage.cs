b1637334f5330f56e1710933a0e915352b37228a
revoljucioner <moderya7@gmail.com>
2018-11-29 16:40:34 +0200

    ignored

---
+++ Log/Log.Android/Permissions/PermissionsGroupsStorage.cs
@@ -0,0 +1,25 @@
+﻿using Android;
+using Log.DependenciesOS;
+
+namespace Log.Droid.Permissions
+{
+    public class PermissionsGroupsStorage: IPermissionsStorage
+    {
+        public string[] PermissionsLocation => new []
+        {
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation
+
+        };
+
+        public string[] PermissionsPhone => new[]
+        {
+            Manifest.Permission.ReadPhoneState
+        };
+
+        public string[] PermissionsInternet => new[]
+        {
+            Manifest.Permission.Internet
+        };
+    }
+}
